ee3e0268ffaf75445d63ae132ee067272c8a1026
meishixiang <meishixiang@landawang.com>
2018-04-19 17:53:57 +0800

    修改签名为横屏

--- src/BestSignSdk.php
+++ src/BestSignSdk.php
@@ -288,6 +288,7 @@ class BestSignSdk
         $post_data['vcodeMobile'] = $userInfo['mobile'];
         $post_data['pushUrl'] = $pushUrl;
         $post_data['returnUrl'] = $returnUrl;
+        $post_data['version'] = '3'; //签字屏横屏
         $post_data['isDrawSignatureImage'] = '2';//2强制必须手绘签名（只能手写不允许使用默认签名）
 
         $post_data = json_encode($post_data);
